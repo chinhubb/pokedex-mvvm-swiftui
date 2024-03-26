132599ed2c3611518b1e2083dba7c8e3dfeb803b
Jirawat <jirawat.chin.10@gmail.com>
2024-03-26 13:26:00 +0700

    navigator to detail

--- pokedex-mvvm-swiftui/Views/BookmarkedPokemonListView.swift
+++ pokedex-mvvm-swiftui/Views/BookmarkedPokemonListView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct BookmarkedPokemonListView: View {
     @ObservedObject var bookmarkManager: BookmarkManager
     @ObservedObject var pokemonListViewModel = PokemonListViewModel()
-
+    
     var body: some View {
         List {
             ForEach(getBookmarkedPokemon()) { pokemon in
-                // ... your list item view ...
-                Text(pokemon.name)
+                NavigationLink(destination: PokemonDetailsView(viewModel: PokemonDetailsViewModel(pokemon: pokemon, bookmarkManager: bookmarkManager))) {
+                    BookmarkRowView(pokemon: pokemon)
+                }
             }
         }
         .navigationTitle("Bookmarked Pokemon")
@@ -23,10 +24,18 @@ struct BookmarkedPokemonListView: View {
             bookmarkManager.loadBookmarks()
         }
     }
-
+    
     func getBookmarkedPokemon() -> [Pokemon] {
         return pokemonListViewModel.pokemonList.filter { pokemon in
             bookmarkManager.bookmarkedPokemonIds.contains(pokemon.id)
         }
+    }
+}
+
+struct BookmarkRowView: View {
+    let pokemon: Pokemon
+
+    var body: some View {
+        Text(pokemon.name.capitalized)
     }
 }
