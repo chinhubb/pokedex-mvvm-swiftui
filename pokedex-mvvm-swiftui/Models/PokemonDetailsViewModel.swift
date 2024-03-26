e1cd61735c64b8438b73e161c7187cfa89796275
Jirawat <jirawat.chin.10@gmail.com>
2024-03-26 15:33:16 +0700

    implement detail view

--- pokedex-mvvm-swiftui/Models/PokemonDetailsViewModel.swift
+++ pokedex-mvvm-swiftui/Models/PokemonDetailsViewModel.swift
@@ -6,29 +6,64 @@
 //
 
 import Foundation
+import Combine
 
 class PokemonDetailsViewModel: ObservableObject {
     @Published var pokemon: Pokemon
     @Published var isBookmarked: Bool = false
+    @Published var isLoading = false
+    @Published var error: Error? = nil
+    
     private let bookmarkManager: BookmarkManager
-
+    private var subscriptions = Set<AnyCancellable>()
+    
     init(pokemon: Pokemon, bookmarkManager: BookmarkManager) {
         self.pokemon = pokemon
         self.bookmarkManager = bookmarkManager
         self.isBookmarked = bookmarkManager.bookmarkedPokemonIds.contains(pokemon.id)
     }
-
+    
     func toggleBookmark() {
         isBookmarked.toggle()
-
+        
         switch bookmarkManager.bookmarkedPokemonIds.contains(pokemon.id) ?
-                bookmarkManager.removeBookmark(pokemonId: pokemon.id) :
-                bookmarkManager.addBookmark(pokemonId: pokemon.id) {
+        bookmarkManager.removeBookmark(pokemonId: pokemon.id) :
+        bookmarkManager.addBookmark(pokemonId: pokemon.id) {
         case .success():
             print("Bookmark updated successfully")
         case .failure(let error):
             print("Bookmark update error: \(error)")
             isBookmarked.toggle()
         }
+    }
+    
+    func getPokemonStats() {
+        isLoading = true
+        error = nil
+        
+        let urlString = "https://pokeapi.co/api/v2/pokemon/\(pokemon.id)"
+        guard let url = URL(string: urlString) else {
+            error = URLError(.badURL)
+            isLoading = false
+            return
+        }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: Pokemon.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                self?.isLoading = false
+                switch completion {
+                case .failure(let error):
+                    self?.error = error
+                case .finished:
+                    break
+                }
+            }, receiveValue: { [weak self] updatedPokemon in
+                self?.pokemon.stats = updatedPokemon.stats
+                print("sdfsf",self?.pokemon.hp)
+            })
+            .store(in: &subscriptions)
     }
 }
--- pokedex-mvvm-swiftui/Views/PokemonDetailsView.swift
+++ pokedex-mvvm-swiftui/Views/PokemonDetailsView.swift
@@ -11,24 +11,76 @@ struct PokemonDetailsView: View {
     @ObservedObject var viewModel: PokemonDetailsViewModel
     
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: viewModel.pokemon.imageUrl)) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: viewModel.pokemon.imageUrl)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                
+                Text(viewModel.pokemon.name.capitalized)
+                    .font(.title)
+                
+                HStack {
+                    Text("HP:")
+                    Text("\(viewModel.pokemon.hp)")
+                        .bold()
+                }
+                
+                HStack {
+                    Text("Attack:")
+                    //                        Text("\(viewModel.pokemon.attack)")
+                    //                            .bold()
+                }
+                
+                HStack {
+                    Text("Defense:")
+                    //                        Text("\(viewModel.pokemon.defense)")
+                    //                            .bold()
+                }
+                
+                HStack {
+                    Text("Special Attack:")
+                    //                        Text("\(viewModel.pokemon.specialAttack)")
+                    //                            .bold()
+                }
+                
+                HStack {
+                    Text("Special Defense:")
+                    //                        Text("\(viewModel.pokemon.specialDefense)")
+                    //                            .bold()
+                }
+                
+                HStack {
+                    Text("Speed:")
+                    //                        Text("\(viewModel.pokemon.speed)")
+                    //                            .bold()
+                }
+                
+                Text("Abilities:")
+                //                    ForEach(viewModel.pokemon.abilities, id: \.name) { ability in
+                //                        Text(ability.name.capitalized)
+                //                    }
+                
+                //                    if viewModel.isLoading {
+                //                        ProgressView()
+                //                    } else if let error = viewModel.error {
+                //                        Text("Error: \(error.localizedDescription)")
+                //                    }
             }
-            
-            Text(viewModel.pokemon.name.capitalized)
-                .font(.title)
         }
+        .onAppear {
+                  viewModel.getPokemonStats()
+              }
         .navigationTitle(viewModel.pokemon.name.capitalized)
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing:
-            Button(viewModel.isBookmarked ? "Unbookmark" : "Bookmark") {
-                viewModel.toggleBookmark()
-            }
+                                Button(viewModel.isBookmarked ? "Unbookmark" : "Bookmark") {
+            viewModel.toggleBookmark()
+        }
         )
     }
 }
